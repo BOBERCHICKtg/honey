84789f2a141d650a86ed1e725f0b65e92c9a4432
BOBERCHICKtg <https://github.com/BOBERCHICKtg/HelloWorld.git>
2025-01-25 13:07:10 +0300

    first commit

---
+++ button.js
@@ -0,0 +1,19 @@
+let body = document.querySelector("body");
+let bHeight = body.getBoundingClientRect().height;
+let screenY = window.innerHeight / 2;
+let btn = document.querySelector("#btn");
+
+window.onscroll = function () {
+    if (pageYOffset > screenY) {
+        btn.classList.add("fixed");
+    } else {
+        btn.classList.remove("fixed");
+    }
+}
+
+btn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+}
